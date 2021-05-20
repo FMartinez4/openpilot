012d4d114f8cd14439ea7da3287e0f2bff4a5636
Francisco Martinez <franciscomartinez919@gmail.com>
2021-05-19 21:05:20 -0500

    Update minSteerSpeed for PACIFICA_2018 to 17.5. Reset gone_fast_yet for PACIFICA_2018.

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -30,11 +30,13 @@ class CarController():
     self.steer_rate_limited = new_steer != apply_steer
 
     moving_fast = CS.out.vEgo > CS.CP.minSteerSpeed  # for status message
+
     if CS.out.vEgo > (CS.CP.minSteerSpeed - 0.5):  # for command high bit
       self.gone_fast_yet = True
-    elif self.car_fingerprint in (CAR.PACIFICA_2019_HYBRID, CAR.PACIFICA_2020, CAR.JEEP_CHEROKEE_2019):
+    elif self.car_fingerprint in (CAR.PACIFICA_2018, CAR.PACIFICA_2019_HYBRID, CAR.PACIFICA_2020, CAR.JEEP_CHEROKEE_2019):
       if CS.out.vEgo < (CS.CP.minSteerSpeed - 3.0):
         self.gone_fast_yet = False  # < 14.5m/s stock turns off this bit, but fine down to 13.5
+
     lkas_active = moving_fast and enabled
 
     if not lkas_active:
